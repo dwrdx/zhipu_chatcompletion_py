0c8ab0db53e3ce88aed42500ff385c1850b89e4f
Edward XU <edward.xu@hotmail.com>
2024-02-27 20:12:08 +0800

    updated server for scf

--- server/flask_server_chatcompletion.py
+++ server/flask_server_chatcompletion.py
@@ -118,7 +118,7 @@ def end_chat():
 
 if __name__ == '__main__':
     try:
-        app.run(debug=True)
+        app.run(host='0.0.0.0', port=9000)
     finally:
         # Here we should not save the conversation because we don't have a user_name
         pass  
